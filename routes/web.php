a083b4faf5604ec263cfb8b212e2c2f0de80d5fa
Esra Latifi <el31758@seeu.edu.mk>
2025-10-30 10:38:17 +0100

    Week 7 updated code-Database week

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
+
+Route::resource('jobs', JobController::class);
+
